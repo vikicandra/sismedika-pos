86e32fecc7a73eb2d1930fb1c7364bb8892bc4f0
Viki Candra <vikicandrata13@gmail.com>
2025-09-05 10:10:53 +0700

    fix: make confirmation dialog component

---
+++ resources/js/components/confirmation-dialog.tsx
@@ -0,0 +1,46 @@
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Alert from "@mui/material/Alert";
+interface ConfirmationDialogProps {
+  open: boolean;
+  title: string;
+  message: string;
+  error?: string | null;
+  onAgree: () => void;
+  onDisagree: () => void;
+}
+
+export default function ConfirmationDialog({
+  open,
+  title,
+  message,
+  error,
+  onAgree,
+  onDisagree,
+}: ConfirmationDialogProps) {
+  return (
+    <Dialog open={open} onClose={onDisagree}>
+      <DialogTitle>{title}</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          {message}
+          {error && (
+            <Alert severity="error" sx={{ mt: 2 }}>
+              {error}
+            </Alert>
+          )}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onDisagree}>Disagree</Button>
+        <Button onClick={onAgree} autoFocus>
+          Agree
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
